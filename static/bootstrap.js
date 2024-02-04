44e56f422c79663fd26ed85e0f20f3a932f47f4b
JesssseL <146840057+JesssseL@users.noreply.github.com>
2024-02-04 01:34:59 +0000

    Adding Favicon and Simple Layout Changes

--- static/bootstrap.js
+++ static/bootstrap.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
   $('#alertToast').toast('show');
 });
 
+//allow carousel to be scrolled
 $(document).ready(function () {
   $('#carouselExample').carousel();
 });
+
+//loading screen stuff
+let myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("homePage").style.display = "block";
+}
